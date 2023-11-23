4f186fddf0050ea051426bdb7d31c503dbb37670
Nutchanol Pharos <nutchanol.pharos@gmail.com>
2023-11-23 17:40:05 +0700

    Create CPN_Update_warehouse.js

---
+++ cypress/e2e/CPN/CPN_Update_warehouse.js
@@ -0,0 +1,7 @@
+import * as random from '../random.js';
+describe('template spec', () => {
+  it('passes', () => {
+    //เข้าเว็บและล็อคอิน
+    cy.visit('https://vch-dev.smoothdark.com/en/applicant')
+  })
+})
